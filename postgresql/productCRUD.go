7bfdd83b7852cde7061bdf94261892f03511a441
ethmore <cetinermehmetali@gmail.com>
2022-08-21 17:28:35 +0300

    Added functions > GetSellerProduct, GetAllProducts, GetProduct

--- postgresql/productCRUD.go
+++ postgresql/productCRUD.go
@@ -1,5 +1,12 @@
 package postgresql
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"strconv"
+)
+
 func InsertProduct(sellerMail, title, price, description, photo, stock string) int {
 	sellerId, _, _ := GetSeller(sellerMail)
 
@@ -25,4 +32,96 @@ func DeleteProduct(id string) int {
 	_, e := db.Exec(deleteStmt, id)
 	CheckError(e)
 	return 200
+}
+
+type Product struct {
+	Id          string
+	Title       string
+	Price       string
+	Description string
+	Image       string
+	Stock       string
+}
+
+func GetSellerProducts(eMail string) ([]Product, error) {
+	id, _, _ := GetSeller(eMail)
+
+	rows, err := db.Query("SELECT id, title, price, description, photo, stock FROM products WHERE sellerid=$1", id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, err
+		} else {
+			log.Fatal(err)
+		}
+	}
+	defer rows.Close()
+
+	var products []Product
+
+	for rows.Next() {
+		var product Product
+		err := rows.Scan(&product.Id, &product.Title, &product.Price, &product.Description, &product.Image, &product.Stock)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		products = append(products, product)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(products)
+	return products, nil
+}
+
+func GetAllProducts() ([]Product, error) {
+	fmt.Println("A")
+
+	rows, err := db.Query("SELECT id, title, price, description, photo, stock FROM products")
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, err
+		} else {
+			log.Fatal(err)
+		}
+	}
+	defer rows.Close()
+
+	var products []Product
+
+	for rows.Next() {
+		var product Product
+		err := rows.Scan(&product.Id, &product.Title, &product.Price, &product.Description, &product.Image, &product.Stock)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		products = append(products, product)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(products)
+	return products, nil
+}
+
+func GetProduct(id string) (*Product, error) {
+	var product Product
+	idInt, _ := strconv.Atoi(id)
+
+	err := db.QueryRow("SELECT id, title, price, description, photo, stock FROM products WHERE id=$1", idInt).Scan(&product.Id, &product.Title, &product.Price, &product.Description, &product.Image, &product.Stock)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, err
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println(product)
+	return &product, nil
 }
