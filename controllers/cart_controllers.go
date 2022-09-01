04fd5e20a7d9b75577be71dd99a4da934f13d572
ethmore <cetinermehmetali@gmail.com>
2022-09-01 22:31:48 +0300

    new controller and repository for cart service

---
+++ controllers/cart_controllers.go
@@ -0,0 +1,310 @@
+package controllers
+
+import (
+	"e-comm/authService/middleware"
+	"e-comm/authService/repositories/mongodb"
+	"e-comm/authService/repositories/postgresql"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+)
+
+type CartRequest struct {
+	Token string
+	Id    string
+	Qty   string
+}
+
+type Body struct {
+	Token string
+}
+
+type PurchaseBody struct {
+	Token      string
+	TotalPrice string
+}
+
+type Item struct {
+	Id         string
+	Title      string
+	TotalPrice string
+}
+
+type CartInfo struct {
+	Id             string
+	Items          []Item
+	TotalCartPrice string
+}
+
+func AddProductToCart() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		auth, authErr := middleware.UserAuth(ctx)
+		if authErr != nil {
+			fmt.Println("auth: ", authErr)
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": "auth error"})
+			return
+		}
+
+		var cartRequest CartRequest
+		if bodyErr := ctx.ShouldBindBodyWith(&cartRequest, binding.JSON); bodyErr != nil {
+			fmt.Println("body: ", bodyErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+		// mongodb.NewCart(auth.EMail)
+		addErr := mongodb.AddProductToCart(auth.EMail, cartRequest.Id, cartRequest.Qty)
+		if addErr != nil {
+			fmt.Println("mongodb (add): ", addErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
+	}
+}
+
+func GetCartInfo() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var body Body
+		if bodyErr := ctx.ShouldBindBodyWith(&body, binding.JSON); bodyErr != nil {
+			fmt.Println("body: ", bodyErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		auth, authErr := middleware.UserAuth(ctx)
+		if authErr != nil {
+			fmt.Println("auth: ", authErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		cart, err := mongodb.FindAllCartProducts(auth.EMail)
+		if err != nil {
+			fmt.Println("mongodb (find): ", err)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		var cartInfo CartInfo
+		for i := 0; i < len(cart.Products); i++ {
+			product, getErr := postgresql.GetProduct(cart.Products[i].Id)
+			if getErr != nil {
+				fmt.Println("postgresql (get): ", getErr)
+				ctx.Status(http.StatusInternalServerError)
+				return
+			}
+
+			itemQty, convErr := strconv.Atoi(cart.Products[i].Qty)
+			if convErr != nil {
+				fmt.Println("convErr: ", convErr)
+				ctx.Status(http.StatusInternalServerError)
+				return
+			}
+			itemPrice, convErr2 := strconv.Atoi(product.Price)
+			if convErr2 != nil {
+				fmt.Println("convErr2: ", convErr2)
+				ctx.Status(http.StatusInternalServerError)
+				return
+			}
+			totalItemPrice := itemQty * itemPrice
+			totalItemPriceStr := strconv.Itoa(totalItemPrice)
+
+			var newProduct = Item{
+				Id:         product.Id,
+				Title:      product.Title,
+				TotalPrice: totalItemPriceStr,
+			}
+
+			cartInfo.Items = append(cartInfo.Items, newProduct)
+		}
+
+		cartInfo.Id = cart.Id
+		cartInfo.TotalCartPrice = cart.TotalPrice
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK", "cartInfo": cartInfo})
+	}
+}
+
+func GetCartProducts() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var body Body
+		if bodyErr := ctx.ShouldBindBodyWith(&body, binding.JSON); bodyErr != nil {
+			fmt.Println("body: ", bodyErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		auth, authErr := middleware.UserAuth(ctx)
+		if authErr != nil {
+			fmt.Println("auth: ", authErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		cart, err := mongodb.FindAllCartProducts(auth.EMail)
+		if err != nil {
+			fmt.Println("mongodb (find): ", err)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK", "products": cart.Products})
+	}
+}
+
+func RemoveProductFromCart() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		auth, authErr := middleware.UserAuth(ctx)
+		if authErr != nil {
+			fmt.Println("auth: ", authErr)
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": "auth error"})
+			return
+		}
+
+		var cartReq CartRequest
+		if bodyErr := ctx.ShouldBindBodyWith(&cartReq, binding.JSON); bodyErr != nil {
+			fmt.Println("body: ", bodyErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		removeErr := mongodb.RemoveProductFromCart(auth.EMail, cartReq.Id)
+		if removeErr != nil {
+			fmt.Println("mongodb (remove): ", removeErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
+	}
+}
+
+func IncreaseProductQty() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var body CartRequest
+		if bodyErr := ctx.ShouldBindBodyWith(&body, binding.JSON); bodyErr != nil {
+			fmt.Println("body: ", bodyErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		auth, authErr := middleware.UserAuth(ctx)
+		if authErr != nil {
+			fmt.Println("auth: ", authErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		incErr := mongodb.IncreaseProductQty(auth.EMail, body.Id)
+		if incErr != nil {
+			fmt.Println("mongodb (inc): ", incErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
+	}
+}
+
+func DecreaseProductQty() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var body CartRequest
+		if bodyErr := ctx.ShouldBindBodyWith(&body, binding.JSON); bodyErr != nil {
+			fmt.Println("body: ", bodyErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		auth, authErr := middleware.UserAuth(ctx)
+		if authErr != nil {
+			fmt.Println("auth: ", authErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		decErr := mongodb.DecreaseProductQty(auth.EMail, body.Id)
+		if decErr != nil {
+			fmt.Println("mongodb (dec): ", decErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK"})
+	}
+}
+
+func AddTotalToCart() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var purchaseBody PurchaseBody
+		if bodyErr := ctx.ShouldBindBodyWith(&purchaseBody, binding.JSON); bodyErr != nil {
+			fmt.Println("body: ", bodyErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		auth, authErr := middleware.UserAuth(ctx)
+		if authErr != nil {
+			fmt.Println("auth: ", authErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		if err := mongodb.AddTotalToCart(auth.EMail, purchaseBody.TotalPrice); err != nil {
+			fmt.Println("add total to cart: ", err)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		ctx.Status(http.StatusOK)
+	}
+}
+
+func GetTotalPrice() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var body PurchaseBody
+
+		if bodyErr := ctx.ShouldBindBodyWith(&body, binding.JSON); bodyErr != nil {
+			fmt.Println("body: ", bodyErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		auth, authErr := middleware.UserAuth(ctx)
+		if authErr != nil {
+			fmt.Println("auth: ", authErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		totalPrice, err := mongodb.GetTotalPrice(auth.EMail)
+		if err != nil {
+			fmt.Println("add total to cart: ", err)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "OK", "totalPrice": totalPrice})
+	}
+}
+
+func ClearCart() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		auth, authErr := middleware.UserAuth(ctx)
+		if authErr != nil {
+			fmt.Println("auth: ", authErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		clrErr := mongodb.ClearCart(auth.EMail)
+		if clrErr != nil {
+			fmt.Println("mongodb (clear): ", clrErr)
+			ctx.Status(http.StatusInternalServerError)
+			return
+		}
+
+		ctx.Status(http.StatusOK)
+	}
+}
---
+++ repositories/mongodb/user_cart_repository.go
@@ -0,0 +1,220 @@
+package mongodb
+
+import (
+	"context"
+	"e-comm/authService/repositories/postgresql"
+	"errors"
+	"fmt"
+	"strconv"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"golang.org/x/exp/slices"
+)
+
+type Product struct {
+	Id  string
+	Qty string
+}
+
+type Cart struct {
+	Id         string `bson:"_id"`
+	UserId     primitive.ObjectID
+	Products   []Product
+	TotalPrice string
+}
+
+func AddProductToCart(userMail, productId, qty string) error {
+	if productId == "" {
+		return errors.New("empty product id")
+	}
+
+	product, getErr := postgresql.GetProduct(productId)
+	if getErr != nil {
+		return getErr
+	}
+
+	user, findErr := FindOneUser(userMail)
+	if findErr != nil {
+		fmt.Println("mongodb (find): ", findErr)
+		return findErr
+	}
+
+	coll := client.Database("eCommUsers").Collection("carts")
+	filter := bson.D{{Key: "userId", Value: user.Id}}
+
+	var result Cart
+	err := coll.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(product)
+	var prod = Product{
+		Id:  productId,
+		Qty: qty,
+	}
+
+	products := append(result.Products, prod)
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "products", Value: products}}}}
+
+	_, updateErr := coll.UpdateOne(context.TODO(), filter, update)
+	if updateErr != nil {
+		return updateErr
+	}
+
+	return nil
+}
+
+func NewCart(userMail string) error {
+	user, findErr := FindOneUser(userMail)
+	if findErr != nil {
+		fmt.Println("mongodb (find): ", findErr)
+		return findErr
+	}
+
+	coll := client.Database("eCommUsers").Collection("carts")
+	doc := bson.D{{Key: "userId", Value: user.Id}, {Key: "products"}}
+
+	_, err := coll.InsertOne(context.TODO(), doc)
+	return err
+}
+
+func FindAllCartProducts(userMail string) (*Cart, error) {
+	user, findErr := FindOneUser(userMail)
+	if findErr != nil {
+		fmt.Println("mongodb (find):", findErr)
+		return nil, findErr
+	}
+	coll := client.Database("eCommUsers").Collection("carts")
+	filter := bson.D{{Key: "userId", Value: user.Id}}
+
+	var result Cart
+	err := coll.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
+func RemoveProductFromCart(userMail, productId string) error {
+	cart, findErr := FindAllCartProducts(userMail)
+	if findErr != nil {
+		fmt.Println("mongodb (remove): ", findErr)
+		return findErr
+	}
+
+	i := slices.IndexFunc(cart.Products, func(c Product) bool { return c.Id == productId })
+
+	copy(cart.Products[i:], cart.Products[i+1:])
+	cart.Products[len(cart.Products)-1] = Product{} // or the zero value of T
+	cart.Products = cart.Products[:len(cart.Products)-1]
+	err := UpdateCartProducts(cart.UserId, cart.Products)
+
+	return err
+}
+
+func UpdateCartProducts(userId primitive.ObjectID, newProducts []Product) error {
+	coll := client.Database("eCommUsers").Collection("carts")
+	filter := bson.D{{Key: "userId", Value: userId}}
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "products", Value: newProducts}}}}
+
+	_, err := coll.UpdateOne(context.TODO(), filter, update)
+	return err
+}
+
+func IncreaseProductQty(userMail, productId string) error {
+	cart, findErr := FindAllCartProducts(userMail)
+	if findErr != nil {
+		fmt.Println("mongodb (remove): ", findErr)
+		return findErr
+	}
+
+	i := slices.IndexFunc(cart.Products, func(c Product) bool { return c.Id == productId })
+	qtyInt, toIntErr := strconv.Atoi(cart.Products[i].Qty)
+	if toIntErr != nil {
+		fmt.Println("conversion: ", toIntErr)
+	}
+	qtyInt++
+	qtyStr := strconv.Itoa(qtyInt)
+
+	cart.Products[i].Qty = qtyStr
+	err := UpdateCartProducts(cart.UserId, cart.Products)
+	fmt.Println(cart.Products[i].Qty)
+	return err
+}
+
+func DecreaseProductQty(userMail, productId string) error {
+	cart, findErr := FindAllCartProducts(userMail)
+	if findErr != nil {
+		fmt.Println("mongodb (remove): ", findErr)
+		return findErr
+	}
+
+	i := slices.IndexFunc(cart.Products, func(c Product) bool { return c.Id == productId })
+	qtyInt, toIntErr := strconv.Atoi(cart.Products[i].Qty)
+	if toIntErr != nil {
+		fmt.Println("conversion: ", toIntErr)
+	}
+	qtyInt--
+	qtyStr := strconv.Itoa(qtyInt)
+
+	cart.Products[i].Qty = qtyStr
+	err := UpdateCartProducts(cart.UserId, cart.Products)
+	fmt.Println(cart.Products[i].Qty)
+	return err
+}
+
+func AddTotalToCart(userMail, totalPrice string) error {
+	cart, findErr := FindAllCartProducts(userMail)
+	if findErr != nil {
+		return findErr
+	}
+
+	cart.TotalPrice = totalPrice
+
+	user, findErr := FindOneUser(userMail)
+	if findErr != nil {
+		fmt.Println("mongodb (find):", findErr)
+		return findErr
+	}
+	coll := client.Database("eCommUsers").Collection("carts")
+	filter := bson.D{{Key: "userId", Value: user.Id}}
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "totalPrice", Value: totalPrice}}}}
+
+	_, err := coll.UpdateOne(context.TODO(), filter, update)
+	return err
+}
+
+func GetTotalPrice(userMail string) (string, error) {
+	user, findErr := FindOneUser(userMail)
+	if findErr != nil {
+		fmt.Println("mongodb (find):", findErr)
+		return "", findErr
+	}
+	coll := client.Database("eCommUsers").Collection("carts")
+	filter := bson.D{{Key: "userId", Value: user.Id}}
+	var result Cart
+	err := coll.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(result.TotalPrice)
+	return result.TotalPrice, nil
+}
+
+func ClearCart(userMail string) error {
+	user, findErr := FindOneUser(userMail)
+	if findErr != nil {
+		return findErr
+	}
+
+	coll := client.Database("eCommUsers").Collection("carts")
+	filter := bson.D{{Key: "userId", Value: user.Id}}
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "products", Value: nil}, {Key: "totalPrice", Value: ""}}}}
+
+	_, err := coll.UpdateOne(context.TODO(), filter, update)
+	return err
+}
